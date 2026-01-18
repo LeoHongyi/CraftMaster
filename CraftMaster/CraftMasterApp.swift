579862be38e8187714bea64885f853e3297f9150
leo <wenhualiu.liu@gmail.com>
2026-01-18 10:44:31 +0800

    Initial Commit

---
+++ CraftMaster/CraftMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftMasterApp.swift
+//  CraftMaster
+//
+//  Created by leo on 18/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct CraftMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
